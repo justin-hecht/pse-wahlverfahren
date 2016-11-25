624cfef9515cebb733cde54f3abdb3fe231ff5c9
Skypr <Skypr@users.noreply.github.com>
2016-11-25 13:18:48 +0100

    Das Testprogramm zum anzeigen der Assertions
    Readme ist im Ordner.
    Ich habe es jetzt gerade über den Browser hochgeladen, sodass es sich vielleicht nicht direkt über git einbinden lassen will.
    Zur Not einfach den Ordner downloaden und in die IDE als file system importieren

---
+++ VotingGUI/src/GUI/StartButton.java
@@ -0,0 +1,17 @@
+package GUI;
+
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+public class StartButton extends JButton{
+  
+  public StartButton() {
+    this.setSize(15, 15);
+    //this.setBorderPainted(false);
+    //this.setFocusPainted(false);
+    //this.setContentAreaFilled(true);
+    //this.setBackground(Color.BLACK);
+    repaint();
+  }
+}
